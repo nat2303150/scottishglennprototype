a2387a60448d58f22af875e027b4f5218203527c
NATALIE KNIGHTS <2303150@uad.ac.uk>
2024-11-08 15:47:55 +0000

    skeleton program

--- scottishglennprototype/prototype.Designer.cs
+++ scottishglennprototype/prototype.Designer.cs
@@ -28,12 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            textBox = new TextBox();
+            btnAddAsset = new Button();
+            btnViewAsset = new Button();
+            btnEditAsset = new Button();
+            btnDeleteAsset = new Button();
+            dataGridViewAssets = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewAssets).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox
+            // 
+            textBox.Location = new Point(183, 227);
+            textBox.Name = "textBox";
+            textBox.Size = new Size(352, 23);
+            textBox.TabIndex = 2;
+            // 
+            // btnAddAsset
+            // 
+            btnAddAsset.Location = new Point(183, 256);
+            btnAddAsset.Name = "btnAddAsset";
+            btnAddAsset.Size = new Size(352, 23);
+            btnAddAsset.TabIndex = 1;
+            btnAddAsset.Text = "add asset";
+            btnAddAsset.UseVisualStyleBackColor = true;
+            btnAddAsset.Click += btnAddAsset_Click;
+            // 
+            // btnViewAsset
+            // 
+            btnViewAsset.Location = new Point(183, 285);
+            btnViewAsset.Name = "btnViewAsset";
+            btnViewAsset.Size = new Size(352, 23);
+            btnViewAsset.TabIndex = 3;
+            btnViewAsset.Text = "view asset";
+            btnViewAsset.UseVisualStyleBackColor = true;
+            btnViewAsset.Click += btnViewAsset_Click;
+            // 
+            // btnEditAsset
+            // 
+            btnEditAsset.Location = new Point(183, 314);
+            btnEditAsset.Name = "btnEditAsset";
+            btnEditAsset.Size = new Size(352, 23);
+            btnEditAsset.TabIndex = 4;
+            btnEditAsset.Text = "edit asset";
+            btnEditAsset.UseVisualStyleBackColor = true;
+            btnEditAsset.Click += btnEditAsset_Click;
+            // 
+            // btnDeleteAsset
+            // 
+            btnDeleteAsset.Location = new Point(183, 343);
+            btnDeleteAsset.Name = "btnDeleteAsset";
+            btnDeleteAsset.Size = new Size(352, 23);
+            btnDeleteAsset.TabIndex = 5;
+            btnDeleteAsset.Text = "delete asset";
+            btnDeleteAsset.UseVisualStyleBackColor = true;
+            btnDeleteAsset.Click += btnDeleteAsset_Click;
+            // 
+            // dataGridViewAssets
+            // 
+            dataGridViewAssets.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewAssets.Location = new Point(183, 71);
+            dataGridViewAssets.Name = "dataGridViewAssets";
+            dataGridViewAssets.Size = new Size(352, 150);
+            dataGridViewAssets.TabIndex = 6;
+            // 
+            // prototype
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridViewAssets);
+            Controls.Add(btnDeleteAsset);
+            Controls.Add(btnEditAsset);
+            Controls.Add(btnViewAsset);
+            Controls.Add(btnAddAsset);
+            Controls.Add(textBox);
+            Name = "prototype";
+            Text = "Form1";
+            Load += prototype_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridViewAssets).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox;
+        private Button btnAddAsset;
+        private Button btnViewAsset;
+        private Button btnEditAsset;
+        private Button btnDeleteAsset;
+        private DataGridView dataGridViewAssets;
     }
 }
--- scottishglennprototype/prototype.cs
+++ scottishglennprototype/prototype.cs
@@ -1,10 +1,47 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net;
+using System.Windows.Forms;
+
 namespace scottishglennprototype
 {
     public partial class prototype : Form
     {
+        private string connectionString = "Data Source=tolmount.abertay.ac.uk;Initial Catalog=sql2303150;User ID=mssql2303150;Password=perry-pearl-staff-floral";
+
         public prototype()
         {
             InitializeComponent();
+        }
+
+        private void prototype_Load(object sender, EventArgs e)
+        {
+            loadAssets();
+        }
+
+        private void loadAssets() {
+            //todo
+        }
+            
+        private void btnAddAsset_Click(object sender, EventArgs e)
+        {
+            //todo
+        }
+
+        private void btnViewAsset_Click(object sender, EventArgs e)
+        {
+            //todo
+        }
+
+        private void btnEditAsset_Click(object sender, EventArgs e)
+        {
+            //todo
+        }
+
+        private void btnDeleteAsset_Click(object sender, EventArgs e)
+        {
+            //todo
         }
     }
 }
